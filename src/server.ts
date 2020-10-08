4c96034910e042d7f279ededa1dca47e8d5324bc
Gabriel Davi <gdavi.gd@gmail.com>
2020-10-07 22:16:04 -0300

    Add eslint
    Co-authored-by: Sofia <sofiapatrocinio@hotmail.com>
    Co-authored-by: Micaella <micaella2212@gmail.com>
    Co-authored-by: Pedro Igor <pedroigor.silva@outlook.com>
    
    Signed-off-by: Gabriel Davi <gdavi.gd@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request } from 'express'
 
-const PORT = 3000;
-const HOST = "0.0.0.0";
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Teste");
-});
-app.listen(PORT, HOST);
+const PORT = 3000
+const HOST = '0.0.0.0'
+const app = express()
+app.get('/', (req: Response, res: Request) => {
+  res.send('teste')
+})
+app.listen(PORT, HOST)
