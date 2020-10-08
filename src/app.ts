b60c990c8c109194913d13291227539d4edf2eb1
Gabriel Davi <gdavi.gd@gmail.com>
2020-10-08 20:55:40 -0300

    teste commit

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import 'dotenv/config'
-import express from 'express'
-import './database'
+import "dotenv/config";
+import express from "express";
+import "./database";
 
 class App {
-  constructor () {
-    this.app = express()
-    this.init()
+  constructor() {
+    this.app = express();
+    this.init();
   }
 
-  init (): void {
-    this.app.use('/', (req, res) => {
-      res.send('teste')
-    })
+  init(): void {
+    this.app.use("/", (req, res) => {
+      res.send("teste");
+    });
   }
 }
 
-export default new App().app
+export default new App().app;
