151e7b7deb0876f6aec9a4349c141648eb6e8479
Pedro Igor <pedroigor.silva@outlook.com>
2020-10-09 00:07:08 -0300

    Add decreases relation

---
+++ src/database/migrations/20201009023542-decreases.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('decreases', { 
+      idCollaborator: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model: 'collaborators',
+          key: 'idCollaborator'
+        }
+      },
+      idLot: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+        model: 'lots',
+          key: 'idLot'
+        }
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      time: {
+        type: Sequelize.TIME,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('decreases');
+  }
+};
