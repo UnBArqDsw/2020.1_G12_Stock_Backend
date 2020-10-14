1d459bf51a986130e4e8ba609809c6930150c9a5
micaellagouveia <micaella2212@gmail.com>
2020-10-14 18:43:49 -0300

    Add users factories
    Co-authored-by: Sofia <sofiapatrocinio@hotmail.com>

---
+++ src/models/Admin.ts
@@ -0,0 +1,30 @@
+import { CollaboratorFactory, Collaborator } from './Collaborator'
+
+/* Fábrica dos Usuários */
+
+export class AdminFactory extends CollaboratorFactory {
+    public factoryMethod(): Collaborator {
+        return new Admin()
+    }
+}
+
+
+class Admin implements Collaborator {
+    public getUserData() {
+        const userData = {
+            name: 'João',
+            password: '123456',
+            document: '00123',
+            acessLevel: 'admin',
+            photo: 'url1'
+        }
+        return userData
+    }
+}
+
+/* Recebe uma fábrica */ 
+function clientCode(CollaboratorFactory) {
+    console.log(CollaboratorFactory.someOperation())
+  }
+
+clientCode(new AdminFactory())
---
+++ src/models/Collaborator.ts
@@ -0,0 +1,24 @@
+/* Fábrica dos Usuários */
+
+export abstract class CollaboratorFactory {
+
+    public abstract factoryMethod(): Collaborator;
+
+     public someOperation(): string {
+       const collaborator = this.factoryMethod()
+       const data = collaborator.getUserData().acessLevel
+       return `Create ${data}`
+     }
+}
+
+/* Usuários Concretos */
+
+export interface Collaborator {
+    getUserData(): {
+        name: string
+        password: string
+        document: string
+        acessLevel: string
+        photo: string
+    }
+}
