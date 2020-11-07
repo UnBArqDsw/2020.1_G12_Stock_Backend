a940281e03ad8444e9dba22485daadc0941cc9cb
micaellagouveia <micaella2212@gmail.com>
2020-11-07 18:49:42 -0300

    Fix lint

--- src/database/migrations/20201107161408-collaborator-email.js
+++ src/database/migrations/20201107161408-collaborator-email.js
@@ -2,26 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(
-      'collaborators',
-      'email',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-    )
+    await queryInterface.addColumn('collaborators', 'email', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn(
-      'collaborators',
-      'email',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-    )
-  }
+    await queryInterface.removeColumn('collaborators', 'email', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    });
+  },
 };
